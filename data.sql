8cd9ea949de8cd92e3e55e1fc7319927e63a4f16
Jason Phan <Phanx091@umn.edu>
2018-05-28 21:04:43 -0500

    edit data.sql

--- data.sql
+++ data.sql
@@ -16,3 +16,9 @@ CREATE TABLE "feedback" (
 -- Sample feedback item
 INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
 VALUES (4, 4, 5, 'Doing Great!');
+
+-- return all data
+SELECT * FROM "feedback";
+
+-- test delete
+DELETE FROM "feedback" WHERE "id" = 8;
