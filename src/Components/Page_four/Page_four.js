462b08cca90f4712388a22f8bc3061a67b39dc37
Jason Phan <Phanx091@umn.edu>
2018-05-30 16:47:42 -0500

    clean up code and spacing

--- src/Components/Page_four/Page_four.js
+++ src/Components/Page_four/Page_four.js
@@ -1,49 +1,59 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-
-const mapReduxStateToProps = (reduxState) => (
-    {reduxState}
-); // end of mapReduxStateToProps
+const mapReduxStateToProps = reduxState => ({ reduxState }); // end of mapReduxStateToProps
 
 class PageFour extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            commentText: '',
-        }
-    } // end of constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentText: ""
+    };
+  } // end of constructor
 
-    handleChangeForComment = event => { // this function returns the value that pass into it to setSet to commentText
-        const entry = event.target.value
-        this.setState({
-            commentText: entry,
-        })
-    } // end of  handleChangeForComment
+  handleChangeForComment = event => {
+    // this function returns the value that pass into it to setSet to commentText
+    const entry = event.target.value;
+    this.setState({
+      commentText: entry
+    });
+  }; // end of  handleChangeForComment
 
-    commentEntry = () => {
-        const action = { type: "FORM_ENTRY", property: 'comments', payload: this.state.commentText};
-        this.props.dispatch(action);
-        this.props.dispatch({type: "SUBMIT_FEEDBACK"})       
-    } // end of commentEntry
+  commentEntry = () => {
+    const action = {
+      type: "FORM_ENTRY",
+      property: "comments",
+      payload: this.state.commentText
+    };
+    this.props.dispatch(action);
+    this.props.dispatch({ type: "SUBMIT_FEEDBACK" });
+  }; // end of commentEntry
 
-    render() {
-        return (
-            <div>
-                <div className="pages">
-                    <h3>Page: 4 of 4</h3>
-                </div>
-                <h4>Any comments you want to leave?</h4> 
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
-                <input type="text" onChange={this.handleChangeForComment} 
-                    value={this.commentText}
-                    placeholder="write something here..."/>
-                <Link to="/5" style={{textDecoration:'none'}} onClick={this.commentEntry}>NEXT</Link>
-
-            </div>
-        ); // end of return
-    } // end of render
+  render() {
+    return (
+      <div>
+        <div className="pages">
+          <h3>Page: 4 of 4</h3>
+        </div>
+        <h4>Any comments you want to leave?</h4>
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+        <input
+          type="text"
+          onChange={this.handleChangeForComment}
+          value={this.commentText}
+          placeholder="write something here..."
+        />
+        <Link
+          to="/5"
+          style={{ textDecoration: "none" }}
+          onClick={this.commentEntry}
+        >
+          NEXT
+        </Link>
+      </div>
+    ); // end of return
+  } // end of render
 } // end of PageFour
 
 export default connect(mapReduxStateToProps)(PageFour);
